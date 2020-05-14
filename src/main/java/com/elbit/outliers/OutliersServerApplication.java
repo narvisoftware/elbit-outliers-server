07f8a2d052eb639befa05e5f080aba391f6737e3
Mihai Vasilache <mihai_vasilache@yahoo.com>
2020-05-14 11:44:51 +0300

    initial commit

---
+++ src/main/java/com/elbit/outliers/OutliersServerApplication.java
@@ -0,0 +1,13 @@
+package com.elbit.outliers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutliersServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutliersServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elbit/outliers/OutliersServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elbit.outliers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutliersServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
